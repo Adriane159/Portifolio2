07c49d7fb04566eb1dd16790ba6d23d787e74cb2
ADRIANE PEREIRA <adriane.pereira@telemont.com.br>
2024-11-21 03:42:33 -0400

    todo projrto

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 let menuIcon = document.querySelector('#menu-icom');
-let navbar = document.querySelector('navbar');
+let navbar = document.querySelector('.navbar');
 let sections = document.querySelector('section');
 let navlLinks = document.querySelector('header nav a');
 
 
-window.onscroll = ( => {
+window.onscroll = () => {
     sections.forEach(sec =>{
         let top = window.scrollY;
-        let offset =>sec.offserTop -150;
-        let heigth = sec.offsetH
+        let offset = sec.offsetTop -150;
+        let heigth = sec.offsetHeigth;
+        let id = sec.getAttibute('id');
+
+        if(top >= offset && top < offset + heigth){
+            navlLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a
+                [href*=' + id +']').classList.add
+                ('active')
+        
+
+            })
+        }
     })
-})
+}
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('b-x');
+    navbar.classList.toggle('active')
+}
