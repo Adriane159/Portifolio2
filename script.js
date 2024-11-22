cf1d1055aa3734e56bc02a656abdf655ef9a21ed
Adriane159 <adriane159@live.com>
2024-11-22 11:17:11 -0400

    bea 2211

--- script.js
+++ script.js
@@ -4,26 +4,40 @@ let sections = document.querySelector('section');
 let navlLinks = document.querySelector('header nav a');
 
 
-window.onscroll = () => {
-    sections.forEach(sec =>{
-        let top = window.scrollY;
-        let offset = sec.offsetTop -150;
-        let heigth = sec.offsetHeigth;
-        let id = sec.getAttribute('id');
+// window.onscroll = () => {
+//     sections.forEach(sec =>{
+//         let top = window.scrollY;
+//         let offset = sec.offsetTop -150;
+//         let heigth = sec.offsetHeigth;
+//         let id = sec.getAttribute('id');
 
-        if(top >= offset && top < offset + heigth){
-            navlLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a
-                [href*=' + id +' ]').classList.add
-                ('active')
+//         if(top >= offset && top < offset + heigth){
+//             navlLinks.forEach(links => {
+//                 links.classList.remove('active');
+//                 document.querySelector('header nav a
+//                 [href*=' + id +' ]').classList.add
+//                 ('active')
         
 
-            })
-        }
-    })
-}
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('b-x');
-    navbar.classList.toggle('active');
-}
+//             })
+//         }
+//     })
+// }
+// menuIcon.onclick = () => {
+//     menuIcon.classList.toggle('b-x');
+//     navbar.classList.toggle('active');
+// }
+
+const botaoVoltarAoTopo = document.getElementById("voltarAoTopo");
+
+window.onscroll = function(){
+    if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        botaoVoltarAoTopo.style.display = "block";
+    } else {
+        botaoVoltarAoTopo.style.display = "none";
+    }  
+};
+
+ botaoVoltarAoTopo.onclick = function(){
+    window.scrollTo({ top: 0, behavior: 'smooth'});
+ };
