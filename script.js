939d24e4047456444cbf05a91cd84703f3d6fd36
adriane <adriane159@live.com>
2024-11-24 22:44:26 -0400

    atualizao projeto

--- script.js
+++ script.js
@@ -4,29 +4,31 @@ let sections = document.querySelector('section');
 let navlLinks = document.querySelector('header nav a');
 
 
-// window.onscroll = () => {
-//     sections.forEach(sec =>{
-//         let top = window.scrollY;
-//         let offset = sec.offsetTop -150;
-//         let heigth = sec.offsetHeigth;
-//         let id = sec.getAttribute('id');
+window.onscroll = () => {
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let heigth = sec.offsetheigth;
+        let id = sec.getAttribute('id');
 
-//         if(top >= offset && top < offset + heigth){
-//             navlLinks.forEach(links => {
-//                 links.classList.remove('active');
-//                 document.querySelector('header nav a
-//                 [href*=' + id +' ]').classList.add
-//                 ('active')
+        if(top >= offset && top < offset + heigth){
+            navlLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id +' ]').classList.add
+                ('active')
+
+                    
+
         
 
-//             })
-//         }
-//     })
-// }
-// menuIcon.onclick = () => {
-//     menuIcon.classList.toggle('b-x');
-//     navbar.classList.toggle('active');
-// }
+            })
+        }
+    })
+}
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('b-x');
+    navbar.classList.toggle('active');
+}
 
 const botaoVoltarAoTopo = document.getElementById("voltarAoTopo");
 
